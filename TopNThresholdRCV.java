6a993810ef650955fc7a1899921b1cdbb230d356
Hinds-D <deh9999@gmail.com>
2019-02-01 23:25:48 -0500

    Update TopNThresholdRCV.java

--- TopNThresholdRCV.java
+++ TopNThresholdRCV.java
@@ -6,55 +6,6 @@ import java.util.Random;
 import deh.ballot.RankedChoiceBallot;
 
 public class TopNThresholdRCV extends RankedChoiceMethod{
-
-    public static void main (String[] args) {
-        String[] candidates = {"A. Tony Grue", "B. Dick Balls", "C. Total Moron"};
-        LinkedList<Integer> vote1 = new LinkedList<Integer>();
-        vote1.add(0); vote1.add(1); vote1.add(2);
-        RankedChoiceBallot voter1 = new RankedChoiceBallot(vote1, candidates);
-        LinkedList<Integer> vote2 = new LinkedList<Integer>();
-        vote2.add(1); vote2.add(2); vote2.add(0);
-        RankedChoiceBallot voter2 = new RankedChoiceBallot(vote2, candidates);
-        LinkedList<Integer> vote3 = new LinkedList<Integer>();
-        vote3.add(2); vote3.add(0); vote3.add(1);
-        RankedChoiceBallot voter3 = new RankedChoiceBallot(vote3, candidates);
-        RankedChoiceBallot[] box = new RankedChoiceBallot[29];
-        for(int i = 0; i < 9; i++) {
-            box[i]=voter1;
-        }
-        for(int i = 0; i < 12; i++) {
-            box[i+9]=voter2;
-        }
-        for(int i = 0; i < 8; i++) {
-            box[i+21]=voter3;
-        }
-        TopNThresholdRCV BadIRVCase = new TopNThresholdRCV(box, candidates);
-        System.out.println("Rank:\tNeed:\tGrue\tBalls\tMoron");
-        System.out.println(BadIRVCase.verbose());
-        System.out.println("Winner:"+BadIRVCase.candidates[BadIRVCase.getWinner()]);
-        RankedChoiceBallot[] liarBox = new RankedChoiceBallot[19];
-        vote1.clear();
-        vote1.add(0); vote1.add(1); vote1.add(2);
-        vote2.clear();
-        vote2.add(1); vote2.add(2); vote2.add(0);
-        vote3.clear();
-        vote3.add(2); vote3.add(0); vote3.add(1);
-      
-        for(int i = 0; i < 5; i++) {
-            liarBox[i]=voter1;
-        }
-        for(int i = 0; i < 8; i++) {
-            liarBox[i+5]=voter2;
-        }
-        for(int i = 0; i < 6; i++) {
-            liarBox[i+13]=voter3;
-        }
-        TopNThresholdRCV lyingPays = new TopNThresholdRCV(liarBox, candidates);
-        System.out.print("Rank:\tNeed:\tGrue\tBalls\tMoron\n");
-        System.out.println(lyingPays.verbose());
-        System.out.print("Winner:"+lyingPays.candidates[lyingPays.getWinner()]);
-
-    }
   
     public TopNThresholdRCV(RankedChoiceBallot[] ballots, String[] candidateList) {
         super(ballots, candidateList);
