3fb3573d11696fdb83136cce84d54a9fd0d9bd09
Hinds-D <deh9999@gmail.com>
2019-02-01 23:27:44 -0500

    Update CycleFreeCondorcet.java

--- CycleFreeCondorcet.java
+++ CycleFreeCondorcet.java
@@ -10,50 +10,6 @@ public class CycleFreeCondorcet extends RankedChoiceMethod{
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
-		String[] candidates = {"A. Tony Grue", "B. Dick Balls", "C. Total Moron"};
-        LinkedList<Integer> vote1 = new LinkedList<Integer>();
-        vote1.add(0); vote1.add(1); vote1.add(2);//9x A>B>C
-        RankedChoiceBallot voter1 = new RankedChoiceBallot(vote1, candidates);
-        LinkedList<Integer> vote2 = new LinkedList<Integer>();//12x B>C>A
-        vote2.add(1); vote2.add(2); vote2.add(0);
-        RankedChoiceBallot voter2 = new RankedChoiceBallot(vote2, candidates);
-        LinkedList<Integer> vote3 = new LinkedList<Integer>();//8x C>A>B
-        vote3.add(2); vote3.add(0); vote3.add(1);
-        RankedChoiceBallot voter3 = new RankedChoiceBallot(vote3, candidates);
-        RankedChoiceBallot[] box = new RankedChoiceBallot[29];
-        for(int i = 0; i < 9; i++) {
-            box[i]=voter1;
-        }
-        for(int i = 0; i < 12; i++) {
-            box[i+9]=voter2;
-        }
-        for(int i = 0; i < 8; i++) {
-            box[i+21]=voter3;
-        }
-        CycleFreeCondorcet BadIRVCase = new CycleFreeCondorcet(box, candidates);
-        System.out.println(BadIRVCase.verbose());
-        RankedChoiceBallot[] liarBox = new RankedChoiceBallot[19];
-        vote1.clear();
-        vote1.add(0); vote1.add(1); vote1.add(2);
-        vote2.clear();
-        vote2.add(1); vote2.add(2); vote2.add(0);
-        vote3.clear();
-        vote3.add(2); vote3.add(0); vote3.add(1);
-      
-        for(int i = 0; i < 5; i++) {
-            liarBox[i]=voter1;
-        }
-        for(int i = 0; i < 8; i++) {
-            liarBox[i+5]=voter2;
-        }
-        for(int i = 0; i < 6; i++) {
-            liarBox[i+13]=voter3;
-        }
-        CycleFreeCondorcet lyingPays = new CycleFreeCondorcet(liarBox, candidates);
-        System.out.println(lyingPays.verbose());
-	}
-
 	@Override
 	public int getWinner() {
 		int[][] pairwiseMatrix = super.getPairwiseTable(super.votes);
