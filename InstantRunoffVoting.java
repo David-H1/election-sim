3aaafadae308d9fba50fd24641f8edd56b5cb4aa
Hinds-D <deh9999@gmail.com>
2019-02-01 23:22:56 -0500

    Update InstantRunoffVoting.java

--- InstantRunoffVoting.java
+++ InstantRunoffVoting.java
@@ -8,65 +8,6 @@ import java.util.Random;
 import deh.ballot.RankedChoiceBallot;
 
 public class InstantRunoffVoting extends RankedChoiceMethod{
-    public static void main (String[] args) {
-        boolean going = true;
-        int tries = 0;
-        String[] candidates = {"A. Tony", "Y. Grue", "X. Dick", "W. Balls", "V. Total", "U. Moron", "Z. Hugh"};
-        RankedChoiceBallot[] chaos = new RankedChoiceBallot[52];
-        LinkedList<Integer> type1 = new LinkedList<Integer>();//A (1x)
-        type1.add(0);
-        LinkedList<Integer> type2 = new LinkedList<Integer>();//Y>A (1x)
-        type2.add(1); type2.add(0);
-        LinkedList<Integer> type3 = new LinkedList<Integer>();//X>Y>A (2x)
-        type3.add(2); type3.add(1); type3.add(0);
-        LinkedList<Integer> type4 = new LinkedList<Integer>();//W>X>Y>A (4x)
-        type4.add(3); type4.add(2); type4.add(1); type4.add(0);
-        LinkedList<Integer> type5 = new LinkedList<Integer>();//V>W>X>Y>A (8x)
-        type5.add(4); type5.add(3); type5.add(2); type5.add(1); type5.add(0);
-        LinkedList<Integer> type6 = new LinkedList<Integer>();//U>V>W>X>Y>A (16x)
-        type6.add(5); type6.add(4); type6.add(3); type6.add(2); type6.add(1); type6.add(0);
-        LinkedList<Integer> type7 = new LinkedList<Integer>();//Z (20x)
-        type7.add(6);
-        int i = 0;
-        for(; i < 1; i++) {
-        	RankedChoiceBallot vote = new RankedChoiceBallot((LinkedList<Integer>) type1.clone(), candidates);
-        	chaos[i] = vote;
-        }
-        for(; i < 1+1; i++) {
-        	RankedChoiceBallot vote = new RankedChoiceBallot((LinkedList<Integer>) type2.clone(), candidates);
-        	chaos[i] = vote;
-        }
-        for(; i < 1+1+2; i++) {
-        	RankedChoiceBallot vote = new RankedChoiceBallot((LinkedList<Integer>) type3.clone(), candidates);
-        	chaos[i] = vote;
-        }
-        for(; i < 1+1+2+4; i++) {
-        	RankedChoiceBallot vote = new RankedChoiceBallot((LinkedList<Integer>) type4.clone(), candidates);
-        	chaos[i] = vote;
-        }
-        for(; i < 1+1+2+4+8; i++) {
-        	RankedChoiceBallot vote = new RankedChoiceBallot((LinkedList<Integer>) type5.clone(), candidates);
-        	chaos[i] = vote;
-        }
-        for(; i < 1+1+2+4+8+16; i++) {
-        	RankedChoiceBallot vote = new RankedChoiceBallot((LinkedList<Integer>) type6.clone(), candidates);
-        	chaos[i] = vote;
-        }
-        for(; i < 1+1+2+4+8+16+20; i++) {
-        	RankedChoiceBallot vote = new RankedChoiceBallot((LinkedList<Integer>) type7.clone(), candidates);
-        	chaos[i] = vote;
-        }
-        InstantRunoffVoting chaosScenario2 = new InstantRunoffVoting(chaos, candidates);
-    	while(going) {
-    	tries++;
-        String chaosResult = chaosScenario2.verbose();
-        System.out.println(chaosResult);
-        if(chaosResult.contains("Winner:0")) {
-        	going=false;
-        }
-        }
-    	System.out.print("\n"+ tries);
-    }
     public InstantRunoffVoting(RankedChoiceBallot[] ballots, String[] candidateList) {
         super(ballots, candidateList);
         // TODO Auto-generated constructor stub
