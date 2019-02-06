82025cb61eebf5d7904528979859f7edb4ff02a6
Hinds-D <deh9999@gmail.com>
2019-02-06 16:05:43 -0500

    Update and rename CycleFreeCondorcet.java to ranked-methods/CycleFreeCondorcet.java

--- ranked-methods/CycleFreeCondorcet.java
+++ ranked-methods/CycleFreeCondorcet.java
@@ -1,8 +1,8 @@
-package deh.methods;
+package dh.methods;
 
 import java.util.LinkedList;
 
-import deh.ballot.RankedChoiceBallot;
+import dh.ballot.RankedChoiceBallot;
 
 public class CycleFreeCondorcet extends RankedChoiceMethod{
 	public CycleFreeCondorcet(RankedChoiceBallot[] ballots, String[] candidateList) {
