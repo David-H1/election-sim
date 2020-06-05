c8a830b661cf6a2c13ea5146e84d6c4e47921ef8
Hinds-D <deh9999@gmail.com>
2020-06-05 12:20:49 -0400

    Update Borda.java

--- ranked-methods/Borda.java
+++ ranked-methods/Borda.java
@@ -1,6 +1,6 @@
-package dh.methods;
+package election.methods;
 
-import dh.ballot.RankedChoiceBallot;
+import election.ballot.RankedChoiceBallot;
 
 public class Borda extends RankedChoiceMethod{
     public Borda(RankedChoiceBallot[] ballots, String[] candidateList) {
